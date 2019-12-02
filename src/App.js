67360a2b700a1357e64cd5fb3c012759a9c2310d
Toby <pasqut@amazon.com>
2019-12-01 21:44:16 -0700

    Added a list and note component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
+import List from './components/List'
+import Note from './components/Note'
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <List />
+      <Note />
     </div>
   );
 }
+
 
 export default App;
